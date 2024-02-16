dff1499b853989b85051aabf421504595050f3c1
Luis Otavio <mgluisotavio@gmail.com>
2024-02-15 21:17:49 -0300

    Adicionar arquivos de projeto.

---
+++ Form1 (savepoint).Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Soma2Numeros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.TxtNumero2 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.BtnSomar = new System.Windows.Forms.Button();
+            this.LblTotal = new System.Windows.Forms.Label();
+            this.TxtTotal = new System.Windows.Forms.TextBox();
+            this.TxtNumero1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " ";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(59, 35);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 1;
+            // 
+            // TxtNumero2
+            // 
+            this.TxtNumero2.AutoSize = true;
+            this.TxtNumero2.Location = new System.Drawing.Point(56, 67);
+            this.TxtNumero2.Name = "TxtNumero2";
+            this.TxtNumero2.Size = new System.Drawing.Size(138, 13);
+            this.TxtNumero2.TabIndex = 2;
+            this.TxtNumero2.Text = "Digite Outro Número Inteiro:";
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(59, 98);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 3;
+            // 
+            // BtnSomar
+            // 
+            this.BtnSomar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BtnSomar.Location = new System.Drawing.Point(46, 139);
+            this.BtnSomar.Name = "BtnSomar";
+            this.BtnSomar.Size = new System.Drawing.Size(122, 36);
+            this.BtnSomar.TabIndex = 4;
+            this.BtnSomar.Text = "Calcular a Soma";
+            this.BtnSomar.UseVisualStyleBackColor = false;
+            this.BtnSomar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LblTotal
+            // 
+            this.LblTotal.AutoSize = true;
+            this.LblTotal.Location = new System.Drawing.Point(98, 193);
+            this.LblTotal.Name = "LblTotal";
+            this.LblTotal.Size = new System.Drawing.Size(13, 13);
+            this.LblTotal.TabIndex = 5;
+            this.LblTotal.Text = "0";
+            this.LblTotal.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // TxtTotal
+            // 
+            this.TxtTotal.Location = new System.Drawing.Point(59, 230);
+            this.TxtTotal.Name = "TxtTotal";
+            this.TxtTotal.Size = new System.Drawing.Size(100, 20);
+            this.TxtTotal.TabIndex = 6;
+            this.TxtTotal.Text = "0";
+            this.TxtTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TxtNumero1
+            // 
+            this.TxtNumero1.AutoSize = true;
+            this.TxtNumero1.Location = new System.Drawing.Point(56, 9);
+            this.TxtNumero1.Name = "TxtNumero1";
+            this.TxtNumero1.Size = new System.Drawing.Size(126, 13);
+            this.TxtNumero1.TabIndex = 8;
+            this.TxtNumero1.Text = "Digite um Número Inteiro:";
+            this.TxtNumero1.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 310);
+            this.Controls.Add(this.TxtNumero1);
+            this.Controls.Add(this.TxtTotal);
+            this.Controls.Add(this.LblTotal);
+            this.Controls.Add(this.BtnSomar);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.TxtNumero2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label TxtNumero2;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button BtnSomar;
+        private System.Windows.Forms.Label LblTotal;
+        private System.Windows.Forms.TextBox TxtTotal;
+        private System.Windows.Forms.Label TxtNumero1;
+    }
+}
+
---
+++ Form1 (savepoint).cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soma2Numeros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Variaveis
+
+            int n1, n2, total;
+
+            //Entrada de Dados
+
+            n1 = int.Parse(TxtNumero1.Text);
+            n2 = int.Parse(TxtNumero2.Text);
+
+            //processamento
+
+            total = n1 + n2;
+
+            // Saidas de Dados
+
+            LblTotal.Text = total.ToString();
+            TxtTotal.Text = total.ToString();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
